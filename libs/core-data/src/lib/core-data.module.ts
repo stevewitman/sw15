cb8535aee1718b31cf0f3902ae788b585cfa1933
Steve Witman <stevewitman@gmail.com>
2020-03-09 08:19:29 -0600

    Added http crud methods to projects service

--- libs/core-data/src/lib/core-data.module.ts
+++ libs/core-data/src/lib/core-data.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@nx15/material';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    MaterialModule
+  ]
 })
 export class CoreDataModule {}
