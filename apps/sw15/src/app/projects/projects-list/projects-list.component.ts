c73576e454753290a868ea12c2563b5bc4c7ab99
Steve Witman <stevewitman@gmail.com>
2020-03-09 07:45:49 -0600

    Added components: projects, projects-list, project-details, toolbar, and services: projects

---
+++ apps/sw15/src/app/projects/projects-list/projects-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nx15-projects-list',
+  templateUrl: './projects-list.component.html',
+  styleUrls: ['./projects-list.component.scss']
+})
+export class ProjectsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
